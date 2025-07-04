f20e239995838be1c3180fc37c9a611cbb12615d
SethK99 <sgkloster@gmail.com>
2025-07-03 19:14:19 -0700

    Add Schlick reflectance approximation to dielectric material

--- material.h
+++ material.h
@@ -67,10 +67,10 @@ class dielectric : public material {
             double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
             double sin_theta = std::sqrt(1.0 - cos_theta*cos_theta);
 
-            bool cannot_reflect = ri * sin_theta > 1.0;
+            bool cannot_refract = ri * sin_theta > 1.0;
             vec3 direction;
 
-            if (cannot_reflect)
+            if (cannot_refract || reflectance(cos_theta, ri) > random_double())
                 direction = reflect(unit_direction, rec.normal);
             else
                 direction = refract(unit_direction, rec.normal, ri);
@@ -83,6 +83,13 @@ class dielectric : public material {
         // Refractive index in vacuum or air, or the ration of the material's refractive index over
         // the refractive index of the enclosing media
         double refraction_index;
+
+        static double reflectance(double cosine, double refraction_index) {
+            // Use Schlick's approximation for reflectance.
+            auto r0 = (1 - refraction_index) / (1 + refraction_index);
+            r0 = r0*r0;
+            return r0 + (1-r0)*std::pow((1 - cosine),5);
+        }
 };
 
 #endif
